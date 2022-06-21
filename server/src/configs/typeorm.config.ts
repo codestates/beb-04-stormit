1f29548e5bb04c79fe16d2a7964e239751bc1a3a
FdongFdong <abnormal5626@naver.com>
2022-06-22 03:24:28 +0900

    로그추가

--- server/src/configs/typeorm.config.ts
+++ server/src/configs/typeorm.config.ts
@@ -1,13 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Board } from 'src/board/entity/board.entity';
+import * as config from 'config';
+const dbConfig = config.get('db');
 
 export const typeORMConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '0410',
-  database: 'stormit_test',
+  type: dbConfig.type,
+  host: dbConfig.host,
+  port: dbConfig.port,
+  username: dbConfig.username,
+  password: dbConfig.password,
+  database: dbConfig.database,
   entities: [Board], // 사용할 entity의 클래스명을 넣어둔다.
-  synchronize: true, // false로 해두는 게 안전하다.
+  synchronize: dbConfig.synchronize, // false로 해두는 게 안전하다.
 };
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,11 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as config from 'config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = 3000;
+  const serverConfig = config.get('server');
+  const PORT = serverConfig.port;
   await app.listen(PORT);
   Logger.log(`Application running on port ${PORT}`);
 }
