52bfee98ce580117ddaefc0c2b130d1f1c284c75
js_lee <jongsuklee45@gmail.com>
2022-06-23 16:08:08 +0900

    token authorization

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
+import { JwtStrategy } from './security/passport.jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [TypeOrmModule.forFeature([UserRepository]),
+  JwtModule.register({
+    secret: 'SECRET_KEY',
+    signOptions : {expiresIn : '400s'}
+  })  ,
+  PassportModule
+],
   exports: [TypeOrmModule],
   controllers: [AuthController],
-  providers: [AuthService, UserService]
+  providers: [AuthService, UserService, JwtStrategy]
 })
 export class AuthModule {}
